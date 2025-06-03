a3c200fb220cb9bf5cd35643f5802288454edfea
mattysplatt <mattwentworth100@gmail.com>
2025-06-03 14:34:19 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+import { INITIALS_DB } from './initials_db.js';
+
+// Firebase Setup (Replace with your config if needed)
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
+import { getDatabase, ref, set, get, onValue, push, remove, update, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1PocQMYJZP0ABWxeoiUNF7C5mHgsDjpk",
+  authDomain: "initialcontact-66089.firebaseapp.com",
+  databaseURL: "https://initialcontact-66089-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "initialcontact-66089",
+  storageBucket: "initialcontact-66089.appspot.com",
+  messagingSenderId: "964931937041",
+  appId: "1:964931937041:web:a6c67c1f3de5d7f55b62b0",
+  measurementId: "G-7LL14YVJCE"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// --- Utility and Game Logic functions here ---
+// (Copy the full code from my previous message's main.js file)
