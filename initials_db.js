7ff6e18b328cf9bb5a6107b9d6fa081a77089080
mattysplatt <mattwentworth100@gmail.com>
2025-06-05 17:23:54 +0800

    Update initials_db.js

--- initials_db.js
+++ initials_db.js
@@ -118,6 +118,348 @@ export const INITIALS_DB = {
       "Brother of Elijah Hollands"
     ]
   }
+    [
+  {
+    "initials": "J.D.",
+    "answer": "Jordan Dawson",
+    "clues": [
+      "Adelaide Crows captain",
+      "Versatile defender/midfielder",
+      "Known for elite kicking skills",
+      "Former Sydney Swans player",
+      "Debuted in 2016"
+    ]
+  },
+  {
+    "initials": "H.A.",
+    "answer": "Harris Andrews",
+    "clues": [
+      "Brisbane Lions co-captain",
+      "Key position defender",
+      "Two-time All-Australian",
+      "Known for intercept marking",
+      "Debuted in 2015"
+    ]
+  },
+  {
+    "initials": "C.C.",
+    "answer": "Charlie Curnow",
+    "clues": [
+      "Carlton Blues key forward",
+      "2023 Coleman Medalist",
+      "Known for athleticism and marking",
+      "Brother of Ed Curnow",
+      "Debuted in 2016"
+    ]
+  },
+  {
+    "initials": "D.M.",
+    "answer": "Darcy Moore",
+    "clues": [
+      "Collingwood Magpies captain",
+      "Key defender",
+      "Son of club legend Peter Moore",
+      "Known for intercept marking",
+      "Debuted in 2015"
+    ]
+  },
+  {
+    "initials": "Z.M.",
+    "answer": "Zach Merrett",
+    "clues": [
+      "Essendon Bombers captain",
+      "Elite midfielder",
+      "Multiple Crichton Medalist",
+      "Known for consistency and leadership",
+      "Debuted in 2014"
+    ]
+  },
+  {
+    "initials": "A.B.",
+    "answer": "Andrew Brayshaw",
+    "clues": [
+      "Fremantle Dockers midfielder",
+      "2022 AFLPA MVP",
+      "Known for work rate and ball-winning",
+      "Brother of Angus Brayshaw",
+      "Debuted in 2018"
+    ]
+  },
+  {
+    "initials": "P.D.",
+    "answer": "Patrick Dangerfield",
+    "clues": [
+      "Geelong Cats midfielder",
+      "2016 Brownlow Medalist",
+      "Eight-time All-Australian",
+      "Known for explosive power",
+      "Debuted in 2008"
+    ]
+  },
+  {
+    "initials": "T.M.",
+    "answer": "Touk Miller",
+    "clues": [
+      "Gold Coast Suns co-captain",
+      "Midfielder known for endurance",
+      "Two-time All-Australian",
+      "Best and Fairest winner",
+      "Debuted in 2015"
+    ]
+  },
+  {
+    "initials": "J.K.",
+    "answer": "Josh Kelly",
+    "clues": [
+      "GWS Giants midfielder",
+      "Known for elite disposal",
+      "Former No.2 draft pick",
+      "Co-captain in 2022",
+      "Debuted in 2014"
+    ]
+  },
+  {
+    "initials": "J.S.",
+    "answer": "James Sicily",
+    "clues": [
+      "Hawthorn Hawks captain",
+      "Key defender",
+      "Known for intercept marking",
+      "All-Australian in 2022",
+      "Debuted in 2015"
+    ]
+  },
+  {
+    "initials": "M.G.",
+    "answer": "Max Gawn",
+    "clues": [
+      "Melbourne Demons captain",
+      "Ruckman",
+      "2021 Premiership captain",
+      "Multiple All-Australian selections",
+      "Debuted in 2011"
+    ]
+  },
+  {
+    "initials": "J.S.",
+    "answer": "Jy Simpkin",
+    "clues": [
+      "North Melbourne Kangaroos captain",
+      "Midfielder",
+      "Known for speed and agility",
+      "Debuted in 2017",
+      "Former first-round draft pick"
+    ]
+  },
+  {
+    "initials": "C.R.",
+    "answer": "Connor Rozee",
+    "clues": [
+      "Port Adelaide Power captain",
+      "Midfielder/forward",
+      "2023 All-Australian",
+      "Known for flair and goal sense",
+      "Debuted in 2019"
+    ]
+  },
+  {
+    "initials": "T.N.",
+    "answer": "Toby Nankervis",
+    "clues": [
+      "Richmond Tigers co-captain",
+      "Ruckman",
+      "Three-time Premiership player",
+      "Known for physicality",
+      "Debuted in 2014"
+    ]
+  },
+  {
+    "initials": "J.S.",
+    "answer": "Jack Steele",
+    "clues": [
+      "St Kilda Saints captain",
+      "Midfielder",
+      "Two-time Best and Fairest winner",
+      "Known for tackling and leadership",
+      "Debuted in 2015"
+    ]
+  },
+  {
+    "initials": "C.M.",
+    "answer": "Callum Mills",
+    "clues": [
+      "Sydney Swans co-captain",
+      "Midfielder",
+      "2022 All-Australian",
+      "Known for versatility",
+      "Debuted in 2016"
+    ]
+  },
+  {
+    "initials": "O.A.",
+    "answer": "Oscar Allen",
+    "clues": [
+      "West Coast Eagles co-captain",
+      "Key forward",
+      "Known for marking ability",
+      "Debuted in 2018",
+      "Former first-round draft pick"
+    ]
+  },
+  {
+    "initials": "T.L.",
+    "answer": "Tom Liberatore",
+    "clues": [
+      "Western Bulldogs vice-captain",
+      "Midfielder",
+      "Known for clearance work",
+      "Son of Tony Liberatore",
+      "Debuted in 2011"
+    ]
+  },
+  {
+    "initials": "S.W.",
+    "answer": "Sam Walsh",
+    "clues": [
+      "Carlton Blues midfielder",
+      "2019 Rising Star winner",
+      "Known for endurance and ball use",
+      "Debuted in 2019",
+      "Former No.1 draft pick"
+    ]
+  },
+  {
+    "initials": "D.C.",
+    "answer": "Darcy Cameron",
+    "clues": [
+      "Collingwood Magpies ruckman",
+      "Stepped up after Grundy's departure",
+      "Known for marking and versatility",
+      "Debuted in 2018",
+      "Former Sydney Swans player"
+    ]
+  },
+  {
+    "initials": "C.S.",
+    "answer": "Caleb Serong",
+    "clues": [
+      "Fremantle Dockers midfielder",
+      "2020 Rising Star winner",
+      "Known for toughness and skill",
+      "Debuted in 2020",
+      "Former top-10 draft pick"
+    ]
+  },
+  {
+    "initials": "T.S.",
+    "answer": "Tom Stewart",
+    "clues": [
+      "Geelong Cats defender",
+      "Multiple All-Australian selections",
+      "Intercept marking specialist",
+      "Key in 2022 premiership win",
+      "Debuted in 2017"
+    ]
+  },
+  {
+    "initials": "M.R.",
+    "answer": "Matt Rowell",
+    "clues": [
+      "Gold Coast Suns midfielder",
+      "Known for contested ball work",
+      "Injury setbacks early in career",
+      "Debuted in 2020",
+      "Former No.1 draft pick"
+    ]
+  },
+  {
+    "initials": "T.G.",
+    "answer": "Tom Green",
+    "clues": [
+      "GWS Giants midfielder",
+      "Known for size and ball-winning",
+      "Breakout season in 2023",
+      "Debuted in 2020",
+      "Former academy player"
+    ]
+  },
+  {
+    "initials": "D.M.",
+    "answer": "Dylan Moore",
+    "clues": [
+      "Hawthorn Hawks forward",
+      "Known for pressure and goal sense",
+      "Breakout season in 2022",
+      "Debuted in 2018",
+      "Former rookie draft pick"
+    ]
+  },
+  {
+    "initials": "J.V.",
+    "answer": "Jack Viney",
+    "clues": [
+      "Melbourne Demons vice-captain",
+      "Midfielder",
+      "Known for toughness and leadership",
+      "Debuted in 2013",
+      "Son of Todd Viney"
+    ]
+  },
+  {
+    "initials": "H.S.",
+    "answer": "Harry Sheezel",
+    "clues": [
+      "North Melbourne Kangaroos defender",
+      "2023 Rising Star winner",
+      "Known for composure and skill",
+      "Debuted in 2023",
+      "Former top-5 draft pick"
+    ]
+  },
+  {
+    "initials": "Z.B.",
+    "answer": "Zak Butters",
+    "clues": [
+      "Port Adelaide Power midfielder",
+      "2023 All-Australian",
+      "Known for agility and ball use",
+      "Debuted in 2019",
+      "Former first-round draft pick"
+    ]
+  },
+  {
+    "initials": "T.T.",
+    "answer": "Tim Taranto",
+    "clues": [
+      "Richmond Tigers midfielder",
+      "Former GWS Giants player",
+      "Known for ball-winning ability",
+      "Debuted in 2017",
+      "Former top-5 draft pick"
+    ]
+  },
+  {
+    "initials": "J.S.",
+    "answer": "Jack Sinclair",
+    "clues": [
+      "St Kilda Saints defender",
+      "2022 All-Australian",
+      "Known for rebounding and ball use",
+      "Debuted in 2015",
+      "Former rookie draft pick"
+    ]
+  },
+  {
+    "initials": "E.G.",
+    "answer": "Errol Gulden",
+    "clues": [
+      "Sydney Swans midfielder/forward",
+      "2021 Rising Star nominee",
+      "Known for left-foot kicking",
+      "Debuted in 2021",
+      "Academy graduate"
+    ]
+  },
   ],
   movieStars: [
     {
