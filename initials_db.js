9d86afb5fc5d84f57dc01e39c35c9bd34270620a
mattysplatt <mattwentworth100@gmail.com>
2025-06-05 17:23:00 +0800

    Update initials_db.js

--- initials_db.js
+++ initials_db.js
@@ -19,7 +19,105 @@ export const INITIALS_DB = {
       answer: "Dustin Martin",
       clues: ["Richmond Tigers midfielder","Known as 'Dusty'","2017 Brownlow Medalist","Three-time Norm Smith Medalist","Famous for fend-off move"]
     }
-    // ...more
+    {
+    "initials": "P.C.",
+    "answer": "Patrick Cripps",
+    "clues": [
+      "Carlton Blues captain",
+      "2022 Brownlow Medalist",
+      "Dominant inside midfielder",
+      "Known for strength and leadership",
+      "Debuted in 2014"
+    ]
+  },
+  {
+    "initials": "N.D.",
+    "answer": "Nick Daicos",
+    "clues": [
+      "Collingwood Magpies midfielder",
+      "Son of club legend Peter Daicos",
+      "2023 Rising Star winner",
+      "Known for elite ball use",
+      "Debuted in 2022"
+    ]
+  },
+  {
+    "initials": "C.P.",
+    "answer": "Christian Petracca",
+    "clues": [
+      "Melbourne Demons midfielder",
+      "2021 Norm Smith Medalist",
+      "Explosive and powerful player",
+      "Key in 2021 premiership win",
+      "Debuted in 2016"
+    ]
+  },
+  {
+    "initials": "M.B.",
+    "answer": "Marcus Bontempelli",
+    "clues": [
+      "Western Bulldogs captain",
+      "Versatile midfielder/forward",
+      "Multiple All-Australian selections",
+      "Known as 'The Bont'",
+      "Debuted in 2014"
+    ]
+  },
+  {
+    "initials": "L.N.",
+    "answer": "Lachie Neale",
+    "clues": [
+      "Brisbane Lions midfielder",
+      "Two-time Brownlow Medalist",
+      "Elite clearance player",
+      "Former Fremantle Docker",
+      "Debuted in 2012"
+    ]
+  },
+  {
+    "initials": "T.G.",
+    "answer": "Toby Greene",
+    "clues": [
+      "GWS Giants forward",
+      "Team captain",
+      "Known for goal sense and aggression",
+      "Multiple All-Australian selections",
+      "Debuted in 2012"
+    ]
+  },
+  {
+    "initials": "T.S.",
+    "answer": "Tom Stewart",
+    "clues": [
+      "Geelong Cats defender",
+      "Multiple All-Australian selections",
+      "Intercept marking specialist",
+      "Key in 2022 premiership win",
+      "Debuted in 2017"
+    ]
+  },
+  {
+    "initials": "C.W.",
+    "answer": "Chad Warner",
+    "clues": [
+      "Sydney Swans midfielder",
+      "Known for speed and agility",
+      "Breakout season in 2022",
+      "Debuted in 2020",
+      "From Western Australia"
+    ]
+  },
+  {
+    "initials": "O.H.",
+    "answer": "Oliver Hollands",
+    "clues": [
+      "Carlton Blues midfielder",
+      "Top 15 draft pick in 2022",
+      "Known for endurance and work rate",
+      "Debuted in 2023",
+      "Brother of Elijah Hollands"
+    ]
+  }
   ],
   movieStars: [
     {
