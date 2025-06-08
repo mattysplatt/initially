cca9602e45cb47751106f0c932f94093cb42325c
mattysplatt <mattwentworth100@gmail.com>
2025-06-08 20:21:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -337,8 +337,10 @@ function chooseCategory(category) {
         )
       )
     : shuffle([...INITIALS_DB[category]]);
-  const firstQuestion = allQuestions[0];
+  const firstQuestion = allQuestions[0]; 
+  
   state.guess = '';
+  
   set(ref(db, `lobbies/${state.lobbyCode}`), {
     code: state.lobbyCode,
     leader: state.playerId,
