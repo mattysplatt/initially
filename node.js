6d2ed0f65307b069fe7a3def120bb47539178818
mattysplatt <mattwentworth100@gmail.com>
2025-06-09 20:52:39 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,14 @@
-
+const fs = require('fs');
+const data = fs.readFileSync('initials_db.js', 'utf8');
+const answerRegex = /answer:\s*"([^"]+)"/g;
+const answers = {};
+let match;
+while ((match = answerRegex.exec(data)) !== null) {
+  const name = match[1];
+  answers[name] = (answers[name] || 0) + 1;
+}
+for (const [name, count] of Object.entries(answers)) {
+  if (count > 1) {
+    console.log(`Duplicate: ${name} (${count} times)`);
+  }
+}
