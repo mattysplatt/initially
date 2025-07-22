a39d4919647e0ad10d1adb5f4ef5a3f4a15b0773
mattysplatt <mattwentworth100@gmail.com>
2025-07-22 15:25:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ function render() {
   else if (state.screen === 'lobbyCode') renderLobbyCodeScreen();
   else if (state.screen === 'category') renderCategory();
   else if (state.screen === 'countdown') renderCountdown(); // <-- Add this line
-  else if (state.screen === 'game') ();
+  else if (state.screen === 'game') renderGame();
   else if (state.screen === 'scoreboard') renderScoreboard();
   else if (state.screen === 'end') renderEnd();
 }
