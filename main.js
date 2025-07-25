5a4ec0ecd11e3073bea0ec882d231dc0c5bcea04
mattysplatt <mattwentworth100@gmail.com>
2025-07-25 23:23:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1132,6 +1132,7 @@ function renderCategory() {
 function startSinglePlayerGame(category) {
   const allQuestions = shuffle([...INITIALS_DB[category]]);
   const firstQuestion = allQuestions[0];
+  state.mode = 'single'; // ensure mode is set
   state.category = category;
   state.round = 1;
   state.maxRounds = 10;
@@ -1144,6 +1145,7 @@ function startSinglePlayerGame(category) {
   state.scores = {};
   state.scoreboard = [];
   state.usedAnswers = [firstQuestion.answer];
+  state.totalPoints = 0; // <-- add this line for running total
   state.screen = 'countdown'; // <-- show countdown first
   render();
 }
