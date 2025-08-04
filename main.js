c05b7827e03d5dbb8c912d3fe0dc9158533757f5
mattysplatt <mattwentworth100@gmail.com>
2025-08-04 18:34:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1249,9 +1249,9 @@ function renderCategory() {
 
   // Mapping of category to card background image
   const backgroundImages = {
-    worldSports: "DeckBackground.png",
+    worldSports: "worldsports.png",
     AFL: "AFLcatcard.png",
-    movieStars: "worldsports.png",
+    movieStars: "Moviecatcard.png",
     musicians: "Musiccatcard.png",
     PopStars: "Popstarcatcard.png",
     Football: "Footballcatcard.png",
